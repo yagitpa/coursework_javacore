49703f0842f1654e111a36680d96b027d0570a5a
yagitpa <yakov.palchikov@gmail.com>
2025-09-27 13:57:40 +0700

    Initial commit

---
+++ src/main/java/org/skypro/courseworkjavacor/JavacorApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.courseworkjavacor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/courseworkjavacor/JavacorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.courseworkjavacor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
